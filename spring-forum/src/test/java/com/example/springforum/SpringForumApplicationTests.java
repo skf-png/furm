3502bec055622c9768efccd0383ad2d20aa00eaf
song-kangfei <2802648097@qq.com>
2025-11-08 20:57:27 +0800

    初始化

---
+++ spring-forum/src/test/java/com/example/springforum/SpringForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
